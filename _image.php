29525572a13331d8a869bde1c43aacf81dddfc85
Jason Knight <jason@jason-knight-martin.com>
2013-09-02 09:26:02 -0400

    Generated Morguefile HTML

--- _image.php
+++ _image.php
@@ -1,6 +1,6 @@
 
 <div class="image">
-	<img src="images/<?php echo $name ?>" /><br />
+	<img src="images/<?php echo $dir ?>/<?php echo $name ?>" /><br />
 	<div class="img-caption">
 		<?php echo $cpy ?>
 	</div>
--- get_images.php
+++ get_images.php
@@ -3,6 +3,7 @@ $dirs = $dirs = array_filter(glob('./images/*'), 'is_dir');
 foreach ( $dirs as $dir ) {
 	$cpy = file_get_contents( $dir . '/copyright.txt');
 	$images = glob($dir ."/*.jpg");
+	$dir = basename($dir);
 	foreach ( $images as $image ) {
 		$name = basename($image);
 		include "./_image.php";
